629b5f20254db80797499748af0f2097959b86ee
Temetsio <alankoteemu12@gmail.com>
2025-11-19 16:58:09 +0200

    Create header.php

---
+++ Teemu-test.html/header.php
@@ -0,0 +1,13 @@
+<header>
+    <h1>Kurssienhallinta</h1>
+    <nav>
+        <ul>
+            <li><a href="kurssienhallinta.php">Etusivu</a></li>
+            <li><a href="kurssit.php">Kurssit</a></li>
+            <li><a href="ilmoittautumiset.php">Ilmoittautumiset</a></li>
+            <li><a href="opettajat.php">Opettajat</a></li>
+            <li><a href="opiskelijat.php">Opiskelijat</a></li>
+            <li><a href="tilat.php">Tilat</a></li>
+        </ul>
+    </nav>
+</header>
